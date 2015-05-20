83b983fd4f1f623c3e051240108bb3ad5762e378
Cesar HERNANDEZ ANTONIO <hernandez.antonio.cesar19@gmail.com>
2015-05-20 11:20:37 +0200

    Added script SQL to change the charset in the database to utf8, convert the tables to utf8 and manual how to use it

--- install/create_tables_data.sql
+++ install/create_tables_data.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS  `_DATABASE_NAME_`.`Report` (
   `excelOnlyInd` tinyint(1) default NULL,
   `reportDatabaseName` varchar(45) NOT NULL,
   PRIMARY KEY  (`reportID`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
 
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `_DATABASE_NAME_`.`ReportGroupingColumn` (
   `reportGroupingColumnName` varchar(45) NOT NULL,
   `reportGroupingColumnID` int(10) unsigned NOT NULL auto_increment,
   PRIMARY KEY  USING BTREE (`reportGroupingColumnID`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `_DATABASE_NAME_`.`ReportParameter` (
   `parentReportParameterID` int(11) default NULL,
   `parameterSQLRestriction` text,
   PRIMARY KEY  (`reportParameterID`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
 
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `_DATABASE_NAME_`.`ReportSum` (
   `reportGroupingColumnName` varchar(45) default NULL,
   `reportAction` varchar(45) default NULL,
   PRIMARY KEY  (`reportSumID`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 
 DELETE FROM `_DATABASE_NAME_`.Report;
